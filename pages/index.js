f1e12330857f3f87becded1f3744c44c29abad01
nisten <nisten@give2science.com>
2021-10-19 10:51:06 -0400

    package updates

--- pages/index.js
+++ pages/index.js
@@ -16,40 +16,40 @@ export default function Home() {
       <Row>
       
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://d2j6dbq0eux0bg.cloudfront.net/images/26105994/1680111461.jpg" />
+      <Card.Img variant="top" src="https://cdn.vox-cdn.com/thumbor/eaNBvoR6F-4PWMoqUaj_gtJh2Hg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/9254955/5868_IMG004868.jpg" />
         <Card.Body>
         <Card.Title>A hat</Card.Title>
          <Card.Text>
            Merp merp, merp some more
         </Card.Text>
-      <Button variant="primary">BUY DRUGS</Button>
+      <Button variant="primary">BUY That</Button>
      </Card.Body>
    </Card>
    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://d2j6dbq0eux0bg.cloudfront.net/images/26105994/1680111461.jpg" />
+      <Card.Img variant="top" src="http://astronomy.com/~/media/D6F7C58A83A448ED91A186939B7F74D3.jpg" />
         <Card.Body>
         <Card.Title>A hat</Card.Title>
          <Card.Text>
            Merp merp, merp some more
         </Card.Text>
-      <Button variant="primary">BUY DRUGS</Button>
+      <Button variant="primary">BUY This</Button>
      </Card.Body>
    </Card>
    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://d2j6dbq0eux0bg.cloudfront.net/images/26105994/1680111461.jpg" />
+      <Card.Img variant="top" src="https://www.syfy.com/sites/syfy/files/styles/2280x1280_hero/public/cassini_finaldives_artwork.jpg" />
         <Card.Body>
         <Card.Title>A hat</Card.Title>
          <Card.Text>
            Merp merp, merp some more
         </Card.Text>
-      <Button variant="primary">BUY MROE DRUGS</Button>
+      <Button variant="primary">BUY MROE</Button>
      </Card.Body>
    </Card>
    </Row>
 
       <Row> 
         <Button variant='secondary'>
-          WHOLEASSBUTTONROW
+          WHOLEBUTTONROW
         </Button>
       </Row>
       </Container>
